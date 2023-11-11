ab6b3cc45e350271b3f868346b8a1142ece46f81
dpere122 <dpere381@fiu.edu>
2023-11-11 11:55:01 -0500

    Ini commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MainBlockComponent } from './main-block/main-block.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainBlockComponent } from './main-block/main-block.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainBlockComponent
+    MainBlockComponent,
+    ToolBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
